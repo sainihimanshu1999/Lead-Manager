a5a772a8a4f0ebc9c0e285a38f9f051a4f2c5704
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-05-06 22:36:01 +0530

    initialized store

---
+++ src/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Forms from './Forms';
+import Leads from './Leads';
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <Forms/>
+            <Leads/>
+            
+        </Fragment>
+    )
+}
