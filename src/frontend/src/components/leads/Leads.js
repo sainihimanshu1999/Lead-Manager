c9c9b24c6206471f9e5720d0262a1ad0c754efba
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-05-07 18:44:40 +0530

    initalize state

--- src/frontend/src/components/leads/Leads.js
+++ src/frontend/src/components/leads/Leads.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getleads } from "../../actions/leads";
 
 export class Leads extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Lead List</h1>
-            </div>
-        )
-    }
+  static propTypes = {
+    leads: PropTypes.array.isRequired,
+  };
+
+  componentDidMount() {
+    this.props.getLeads();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Lead List</h1>
+      </div>
+    );
+  }
 }
 
-export default Leads
+const mapStateToProps = (state) => ({
+  leads: state.leads.leads,
+});
+
+export default connect(mapStateToProps)(Leads);
